15cda0f609b2cc2929600e32fc20209a367f774e
Leoncio97 <jonathanleoncio97@gmail.com>
2025-02-18 23:29:58 -0300

    adicionado novos arquivos da parte de Prontuario em DTO e Controller na parte do backend-SpringBoot

---
+++ SpringProntoClin/src/main/java/com/application/SpringProntoClin/DTO/RequestProntuario.java
@@ -0,0 +1,26 @@
+package com.application.SpringProntoClin.DTO;
+
+public class RequestProntuario {
+
+    private String descricao;
+    private Long pacienteId;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+}
+
+
+
